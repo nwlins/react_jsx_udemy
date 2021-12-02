f278e6d9eda98ed04f3327150c3c654a1c638442
nwlins <nelsonwlins@gmail.com>
2021-12-02 19:20:10 -0300

    CommentDetail done!

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-// Import the React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from 'faker';
+import CommentDetail from "./CommentDetail";
 
-// Create a react component
 const App = function() {
-    return <div>Hi there!</div>
+  return (
+    <div className="ui container comments">
+      <CommentDetail 
+        author="Sam" 
+        timeAgo="Today at 4:45PM" 
+        text="Nice Blog!" 
+        avatar={faker.image.avatar()} 
+      />
+      <CommentDetail 
+        author="Alex" 
+        timeAgo="Today at 2:00PM" 
+        text="Nice Job!" 
+        avatar={faker.image.avatar()} 
+      />
+      <CommentDetail 
+        author="Jane" 
+        timeAgo="Today at 6:00PM" 
+        text="Handjob!" 
+        avatar={faker.image.avatar()} 
+      />
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector('#root'));
-
-//Comment
